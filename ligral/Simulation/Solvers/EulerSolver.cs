d7b27550d0c040dea8ad18cc53eae4a56c3d76d4
Junruoyu Zheng <zhengjry@outlook.com>
2020-11-05 16:38:58 +0800

    fix: integrator delays a frame bug

--- ligral/Simulation/Solvers/EulerSolver.cs
+++ ligral/Simulation/Solvers/EulerSolver.cs
@@ -7,20 +7,18 @@ namespace Ligral.Simulation.Solvers
     {
         public override void Solve(Problem problem)
         {
-            double lastTime = 0;
             List<double> states = problem.InitialValues();
             Settings settings = Settings.GetInstance();
-            for (Time=settings.StepSize; Time<=settings.StopTime; Time+=settings.StepSize)
+            for (Time=0; Time<=settings.StopTime; Time+=settings.StepSize)
             {
                 List<double> stateDerivatives = problem.SystemDynamicFunction(states);
                 states = states.Zip(stateDerivatives).ToList().ConvertAll(pair => 
                 {
                     var state = pair.First;
                     var deriv = pair.Second;
-                    state += deriv*(Time - lastTime);
+                    state += deriv*settings.StepSize;
                     return state;
                 });
-                lastTime = Time;
             }
         }
     }
