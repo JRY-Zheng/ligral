b20385ee1cb43c500f174d206914235aa613392d
Junruoyu Zheng <zhengjry@outlook.com>
2020-12-05 15:27:07 +0800

    add: contact site

--- web/build.py
+++ web/build.py
@@ -1,29 +1,70 @@
-import sys
+import sys, os
 import markdown
 from bs4 import BeautifulSoup
 
-with open('README.md', 'r', encoding='utf8') as f:
-    text = f.read()
+def set_article(soup, article):
+    with open(article, 'r', encoding='utf8') as f:
+        text = f.read()
 
-html_text = markdown.markdown(text, extensions=['markdown.extensions.tables'])
-html = BeautifulSoup(html_text)
+    html_text = markdown.markdown(text, extensions=['markdown.extensions.tables'])
+    html = BeautifulSoup(html_text, features='lxml')
 
-with open('web/index.html', 'r', encoding='utf8') as f:
-    text = f.read()
+    articles = soup.find_all('article')
+    if len(articles) == 0:
+        print('ERROR: no article was found.')
+        sys.exit(1)
+    elif len(articles) > 1:
+        print('WARN: multiple articles were found, use the first.')
+    else:
+        article = articles[0]
+        article.clear()
+        bodies = html.find_all('body')
+        body = bodies[0]
+        for i, child in enumerate(body.children):
+            article.insert(i, child)
 
-soup = BeautifulSoup(text)
-articles = soup.find_all('article')
-if len(articles) == 0:
-    print('ERROR: no article was found.')
-    sys.exit(1)
-elif len(articles) > 1:
-    print('WARN: multiple articles were found, use the first.')
-else:
-    article = articles[0]
-    bodies = html.find_all('body')
-    body = bodies[0]
-    for i, child in enumerate(body.children):
-        article.insert(i, child)
+def delete_aside(soup):
+    asides = soup.find_all('aside')
+    for aside in asides:
+        aside.extract()
+    div = soup.find('div', class_='col-sm-8')
+    div['class'].remove('col-sm-8')
+    div['class'].append('col')
 
-with open('web/index.html', 'w', encoding='utf8') as f:
-    f.write(soup.prettify())
+def delete_jumbotron(soup):
+    jumbotrons = soup.find_all('div', class_='jumbotron-wrap')
+    for jumbotron in jumbotrons:
+        jumbotron.extract()
+
+def set_title(soup, title):
+    title_element = soup.find('title')
+    title = soup.new_string(title)
+    title_element.clear()
+    title_element.insert(0, title)
+
+def set_navbar_active(soup, index):
+    nav_items = soup.find_all('li', class_='nav-item')
+    for i, nav_item in enumerate(nav_items):
+        if 'active' in nav_item['class'] and i != index:
+            nav_item['class'].remove('active')
+        elif 'active ' not in nav_item['class'] and i == index:
+            nav_item['class'].append('active')
+
+if __name__ == "__main__":
+        
+    with open('web/index.html', 'r', encoding='utf8') as f:
+        text = f.read()
+
+    soup = BeautifulSoup(text, features='lxml')
+
+    set_article(soup, os.path.join(os.path.dirname(__file__), '../README.md'))
+    with open('web/index.html', 'w', encoding='utf8') as f:
+        f.write(soup.prettify())
+
+    set_article(soup, os.path.join(os.path.dirname(__file__), 'contact.md'))
+    delete_aside(soup)
+    delete_jumbotron(soup)
+    set_title(soup, '联系我们')
+    set_navbar_active(soup, 3)
+    with open('web/contact.html', 'w', encoding='utf8') as f:
+        f.write(soup.prettify())
---
+++ web/contact.md
@@ -0,0 +1,27 @@
+## 联系我们
+
+我们是一群有志于突破工业软件封锁、具有开源精神的开发者。本项目旨在替代 Simulink 进行仿真，通过与框图等效的文本语言描述仿真对象，并解释/编译而后求解。
+
+### 如果您想阅读仓库代码 ...
+
+您可以去 [Gitee](https://gitee.com/junruoyu-zheng/ligral) 或者 [GitHub](https://github.com/JRY-Zheng/ligral) 上 Fork 我们的仓库，然后通过命令
+
+    git clone https://your-username.gitee.com/ligral.git
+
+将代码克隆到本地。如果您想持续关注仓库动态，可以点击 Watch，如果您觉得本项目还不错，请为我们点一个 Star，十分感谢！
+
+### 如果您发现了一些 Bug ...
+
+或者您有一些建议，或者疑问，您都可以新建一个 [Issue](https://gitee.com/junruoyu-zheng/ligral/issues)。如果您不习惯提出一个 Issue，您也可以[给我写信](mailto:zhengjry@outlook.com)，如果是很有价值的问题或建议，我也会以匿名的形式放在 Issue 中。
+
+### 如果您甚至修改了 Bug ...
+
+或者您改进了我们的代码，那实在是太感谢了！希望您尽快发起一个 PR，我们会对您的 PR 进行审查。不过由衷建议您在发起 PR 前先和我们讨论一下，以免不符合要求，打击您的积极性。讨论的方式可以是 [Issue](https://gitee.com/junruoyu-zheng/ligral/issues) 或者 [email](mailto:zhengjry@outlook.com)。当然，有必要的话我们也可以添加其他联系方式。
+
+### 如果您想关注开发者 ...
+
+目前，项目组仅有我（苦愚君）一个人，你也可以叫我郑君。你可以在[知乎](https://www.zhihu.com/people/LostFish)和[即刻](https://web.okjike.com/u/105ad022-f646-48c0-8236-6007ee5179c5)上关注我，在那里我可能会不定期更新一些本项目的计划、进度和思考。
+
+### 如果您想成为开发者 ...
+
+非常期待您能尽快与我联系，请不要犹豫！无论是[知乎](https://www.zhihu.com/people/LostFish)还是[即刻](https://web.okjike.com/u/105ad022-f646-48c0-8236-6007ee5179c5)上发私信，还是直接[给我写信](mailto:zhengjry@outlook.com)，都是可以的！
