a497b1d00bd6cc2d53e4d2a168f4b098ffbbb9ad
Junruoyu Zheng <zhengjry@outlook.com>
2020-12-05 23:21:39 +0800

    add: image migrate function

--- web/build.py
+++ web/build.py
@@ -1,13 +1,15 @@
-import sys, os
+import sys, os, shutil
 import markdown
 from bs4 import BeautifulSoup
 
-def set_article(soup, article):
+def set_article(soup, article, pre_process=None):
     with open(article, 'r', encoding='utf8') as f:
         text = f.read()
 
     html_text = markdown.markdown(text, extensions=['markdown.extensions.tables'])
     html = BeautifulSoup(html_text, features='lxml')
+    if callable(pre_process):
+        pre_process(html)
 
     articles = soup.find_all('article')
     if len(articles) == 0:
@@ -50,20 +52,47 @@ def set_navbar_active(soup, index):
         elif 'active ' not in nav_item['class'] and i == index:
             nav_item['class'].append('active')
 
+def migrate_img(soup, base_dir):
+    images = soup.find_all('img')
+    for image in images:
+        src = image['src']
+        src_file = os.path.join(base_dir, src)
+        if os.path.exists(src_file):
+            # local image found
+            filename = os.path.basename(src_file)
+            dst_file = f'web/img/{filename}'
+            if os.path.exists(dst_file):
+                print(f'{dst_file} already exists, will be overrode.')
+            try:
+                shutil.copy(src_file, dst_file)
+            except shutil.SameFileError:
+                print('same file, ignored')
+            image['src'] = f'img/{os.path.basename(src_file)}'
+
 if __name__ == "__main__":
+    script_folder = os.path.dirname(__file__)
         
     with open('web/index.html', 'r', encoding='utf8') as f:
         text = f.read()
 
     soup = BeautifulSoup(text, features='lxml')
 
-    set_article(soup, os.path.join(os.path.dirname(__file__), '../README.md'))
+    set_article(soup, os.path.join(script_folder, '../README.md'))
     with open('web/index.html', 'w', encoding='utf8') as f:
         f.write(soup.prettify())
 
-    set_article(soup, os.path.join(os.path.dirname(__file__), 'contact.md'))
-    delete_aside(soup)
-    delete_jumbotron(soup)
+    # witten manually
+
+    # set_article(soup, os.path.join(script_folder, 'product.md'), 
+    #     lambda soup: migrate_img(soup, script_folder))
+    # delete_aside(soup)
+    # delete_jumbotron(soup)
+    # set_title(soup, '产品')
+    # set_navbar_active(soup, 2)
+    # with open('web/product.html', 'w', encoding='utf8') as f:
+    #     f.write(soup.prettify())
+
+    set_article(soup, os.path.join(script_folder, 'contact.md'))
     set_title(soup, '联系我们')
     set_navbar_active(soup, 3)
     with open('web/contact.html', 'w', encoding='utf8') as f:
