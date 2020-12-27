42ae7b946163f58530800805a5b4ff2b64efc9ce
Junruoyu Zheng <zhengjry@126.com>
2020-12-27 10:49:00 +0800

    exm: 1 dof car model, modular design

---
+++ examples/simple-car-1dof/playback_gen.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+time = np.arange(0,10,0.01)
+throttle = np.zeros_like(time)
+brake = np.zeros_like(time)
+throttle[:300] = np.tanh(time[:300])
+brake[300:325] = time[300:325] - time[300]
+brake[325:350] = time[350] - time[325:350]
+throttle[350:900] = 0.4*np.tanh(time[350:900]-time[350])
+brake[900:1000] = np.ones_like(time[900:1000])
+
+# plt.plot(time, throttle)
+# plt.plot(time, brake)
+# plt.show()
+
+with open('examples/simple-car-1dof/throttle.csv', 'w') as f:
+    f.write('Time, Data\n')
+    for t, thr in zip(time, throttle):
+        f.write(f'{t},{thr}\n')
+
+with open('examples/simple-car-1dof/brake.csv', 'w') as f:
+    f.write('Time, Data\n')
+    for t, br in zip(time, brake):
+        f.write(f'{t},{br}\n')
