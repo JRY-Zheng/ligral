da314978deab9f8c000e6b01cb172802655444dc
Junruoyu Zheng <zhengjry@outlook.com>
2021-01-07 16:40:29 +0800

    add: plugin project control for test

---
+++ plugins/control/src/ControlPlugin.cs
@@ -0,0 +1,22 @@
+﻿/* Copyright (C) 2019-2021 Junruoyu Zheng. Home page: https://junruoyu-zheng.gitee.io/ligral
+
+   Distributed under MIT license.
+   See file LICENSE for detail or copy at https://opensource.org/licenses/MIT
+*/
+
+using System;
+using System.Collections.Generic;
+using Ligral.Component;
+using Ligral.Extension;
+using LigralPlugins.Control.Models;
+
+namespace LigralPlugins.Control
+{
+    public class ControlPlugin : IPlugin
+    {
+        public Dictionary<string, Func<Model>> ModelTypePool {get; private set;} = new Dictionary<string, Func<Model>>()
+        {
+            {"TestModel", ()=>new TestModel()}
+        };
+    }
+}
---
+++ plugins/control/src/Models/TestModel.cs
@@ -0,0 +1,30 @@
+/* Copyright (C) 2019-2021 Junruoyu Zheng. Home page: https://junruoyu-zheng.gitee.io/ligral
+
+   Distributed under MIT license.
+   See file LICENSE for detail or copy at https://opensource.org/licenses/MIT
+*/
+
+using System.Collections.Generic;
+using Ligral.Component;
+
+namespace LigralPlugins.Control.Models
+{
+    public class TestModel : Model
+    {
+
+        protected override string DocString
+        {
+            get
+            {
+                return "This model outputs twice the input.";
+            }
+        }
+        protected override List<Signal> DefaultCalculate(List<Signal> values)
+        {
+            Signal inputSignal = values[0];
+            Signal outputSignal = Results[0];
+            outputSignal.Clone(2*inputSignal);
+            return Results;
+        }
+    }
+}
