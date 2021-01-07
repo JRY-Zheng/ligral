6942fdec4f2c1baf2f09e0a60c374d98817e3694
Junruoyu Zheng <zhengjry@outlook.com>
2021-01-07 15:55:57 +0800

    add: plugin interface

---
+++ src/Extension/IPluginable.cs
@@ -0,0 +1,16 @@
+/* Copyright (C) 2019-2021 Junruoyu Zheng. Home page: https://junruoyu-zheng.gitee.io/ligral
+
+   Distributed under MIT license.
+   See file LICENSE for detail or copy at https://opensource.org/licenses/MIT
+*/
+
+using System.Collections.Generic;
+using Ligral.Component;
+
+namespace Ligral.Extension
+{
+    public interface IPlugin
+    {
+        Dictionary<string,System.Func<Model>> ModelTypePool {get;}
+    }
+}
