alg: fix when x length > 2 broadcast error

algorithms/optimization.py
@@ -12,25 +12,26 @@ class Optimizer:
         self.m = 20
 
     def optimize(self, cost, x0, xmax, xmin):
-        self.x0s = x0 + (np.random.rand(self.m,1)*2-1)*(xmax-xmin)/10
-        self.xs = np.copy(self.x0s)
-        self.vs = np.zeros_like(self.xs)
-        self.Pij = np.copy(self.x0s)
-        self.Cij = [cost(x) for x in self.xs]
+        x0 = np.matrix(x0)
+        self.x0s = x0 + (xmax-xmin)/10*(np.random.rand(1,self.m)*2-1)
+        self.xs = self.x0s.copy()
+        self.vs = np.matrix(np.zeros_like(self.xs))
+        self.Pij = self.x0s.copy()
+        self.Cij = [cost(x.T) for x in self.xs.T]
         self.opt = np.argmin(self.Cij)
-        self.Pj = self.Pij[self.opt:self.opt+1]
+        self.Pj = self.Pij[:,self.opt]
         self.Cj = self.Cij[self.opt]
         for g in range(1, self.gmax):
             c1 = self.c1max-g/self.gmax*(self.c1max-self.c1min)
             c2 = self.c2min+g/self.gmax*(self.c2max-self.c2min)
             w = self.wmax-g/self.gmax*(self.wmax-self.wmin)
             for p in range(self.m):
-                self.vs[p] = w*self.vs[p]+c1*np.random.rand()*(self.Pij[p]-self.xs[p])+c2*np.random.rand()*(self.Pj[0]-self.xs[p])
-                self.xs[p] = self.xs[p]+self.vs[p]
-                C = cost(self.xs[p])
+                self.vs[:,p] = w*self.vs[:,p]+c1*np.random.rand()*(self.Pij[:,p]-self.xs[:,p])+c2*np.random.rand()*(self.Pj[:,0]-self.xs[:,p])
+                self.xs[:,p] = self.xs[:,p]+self.vs[:,p]
+                C = cost(self.xs[:,p])
                 if self.Cij[p]>C:
                     self.Cij[p] = C
-                    self.Pij[p] = self.xs[p]
+                    self.Pij[:,p] = self.xs[:,p]
             self.opt = np.argmin(self.Cij)
-            self.Pj = self.Pij[self.opt:self.opt+1]
+            self.Pj = self.Pij[:,self.opt]
             self.Cj = self.Cij[self.opt]

algorithms/test.py
@@ -20,14 +20,20 @@ class Tester:
 
     def test_optimization(self, optimizer:Optimizer) -> bool:
         cost = lambda x: sum(np.sin(x)+np.cos(2*x)*1.5+np.sin(1.3*x+0.3)*0.9+2.5)
-        test_x = np.matrix(np.arange(4.53, 4.56, 0.0001)).T
-        costs = [cost(x) for x in test_x]
+        test_x = np.matrix(np.arange(4.53, 4.56, 0.0001))
+        costs = [cost(x.T) for x in test_x.T]
         index = np.argmin(costs)
-        x_opt = test_x[index]
+        x_opt = test_x[:,index]
         optimizer.optimize(cost, np.matrix(3), np.matrix(-2), np.matrix(3))
         print('the theoretical optimal value is', x_opt, '\nand we got', optimizer.Pj)
         assert (np.abs(optimizer.Pj-x_opt)<self.eps).all()
         print('test passed!')
+
+    def test_trimmer(self, plant:Plant, condition:TrimCondition, trimmer:Trimmer) -> bool:
+        p = trimmer.trim(plant, condition)
+        x = p[:plant.n]
+        u = p[plant.n:]
+        print('x =', x, '\nu =', u)
 
 tester = Tester()
 linearizer = Linearizer()
@@ -36,3 +42,11 @@ tester.test_linearization(linearizer, pendulum)
 
 optimizer = Optimizer()
 tester.test_optimization(optimizer)
+
+condition = TrimCondition(pendulum)
+condition.x0[0,0] = 1
+condition.x_wgt[0,0] = 1
+condition.x_wgt[1,0] = 1
+condition.u_wgt[0,0] = 1
+trimmer = Trimmer(optimizer)
+tester.test_trimmer(pendulum, condition, trimmer)