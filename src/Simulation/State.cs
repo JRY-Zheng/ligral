72064ca62512660262aaee54d731281e8c8064e0
Junruoyu Zheng <zhengjry@126.com>
2021-01-30 17:37:05 +0800

    add: create state and use handle

--- src/Simulation/State.cs
+++ src/Simulation/State.cs
@@ -14,16 +14,42 @@ namespace Ligral.Simulation
         public double StateVariable;
         public double InitialValue;
         public double Derivative;
+        private static Logger logger = new Logger("State");
         public static List<State> StatePool = new List<State>();
+        public static Dictionary<string, StateHandle> StateHandles = new Dictionary<string, StateHandle>();
         public string Name;
         public static State CreateState(string name)
         {
-            State state = new State();
-            StatePool.Add(state);
-            state.Name = name??$"State{StatePool.Count}";
-            return state;
+            name = name??$"State{StatePool.Count}";
+            if (StatePool.Exists(state => state.Name == name))
+            {
+                throw logger.Error(new LigralException($"State {name} has already existed."));
+            }
+            else
+            {
+                State state = new State(name);
+                StatePool.Add(state);
+                return state;
+            }
         }
-        private State() {}
+        public static StateHandle CreateState(string name, int rowNo, int colNo, Signal initial)
+        {
+            name = name??$"State{StatePool.Count}";
+            if (StateHandles.ContainsKey(name))
+            {
+                throw logger.Error(new LigralException($"State handle {name} has already existed."));
+            }
+            else
+            {
+                var handle = new StateHandle(name, rowNo, colNo, initial);
+                StateHandles.Add(name, handle);
+                return handle;
+            }
+        }
+        private State(string name) 
+        {
+            Name = name;
+        }
     }
     public class StateHandle : Handle<State>
     {
