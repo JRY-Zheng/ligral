d17bf5170ea354302519c176923767934c250a80
Junruoyu Zheng <zhengjry@outlook.com>
2021-02-14 14:31:26 +0800

    add: download file, fix host error

--- src/Network/ExampleManager.cs
+++ src/Network/ExampleManager.cs
@@ -27,26 +27,52 @@ namespace Ligral.Network
         private List<GitInfo> ExamplesList;
         public void GetExamplesList()
         {
-            string examples = GetHttpResponse(apiUrl);
-            var examplesInfo = JsonSerializer.Deserialize<GitInfo[]>(examples);
-            ExamplesList = new List<GitInfo>(examplesInfo);
+            using (var examples = GetHttpResponse(apiUrl))
+            {
+                var examplesInfo = JsonSerializer.DeserializeAsync<GitInfo[]>(examples);
+                ExamplesList = new List<GitInfo>(examplesInfo.Result);
+            }
+            DownloadFile("linearization/sys.lig");
         }
-        private string GetHttpResponse(string url)
+        private void DownloadFile(string path)
+        {
+            string fileUrl = downloadUrl+"/"+path;
+            System.Console.WriteLine(fileUrl);
+            using (var rawContentStream = GetHttpResponse(fileUrl))
+            {
+                using (StreamReader reader = new StreamReader(rawContentStream))
+                {
+                    // System.Console.Write(reader.ReadToEnd());
+                    using (FileStream fileStream = new FileStream(path, FileMode.Create))
+                    {
+                        using (StreamWriter writer = new StreamWriter(fileStream))
+                        {
+                            int index = 0;
+                            char[] buffer = new char[1024];
+                            while (!reader.EndOfStream)
+                            {
+                                int count = reader.ReadBlock(buffer, 0, 1024);
+                                writer.Write(buffer, 0, count);
+                                index += 1024;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        private Stream GetHttpResponse(string url)
         {
             ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback(
                 (sender, certificate, chain, error) => true
             );
-            WebRequest request = WebRequest.Create(apiUrl);
+            WebRequest request = WebRequest.Create(url);
             request.Credentials = CredentialCache.DefaultCredentials;
             request.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36");
-            request.Headers.Add("Host", "api.github.com");
+            string host = url.Substring(8);
+            host = host.Substring(0, host.IndexOf('/'));
+            request.Headers.Add("Host", host);
             WebResponse response = request.GetResponse();
-            Stream stream = response.GetResponseStream();
-            StreamReader reader = new StreamReader(stream);
-            string message = reader.ReadToEnd();
-            reader.Close();
-            stream.Close();
-            return message;
+            return response.GetResponseStream();
         }
     }
 }
