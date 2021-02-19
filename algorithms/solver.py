2c8a6d1c6e4c33864af9b3ed9657d29f9ab60d86
Junruoyu Zheng <zhengjry@outlook.com>
2021-02-19 09:35:32 +0800

    alg: implicit solver

--- algorithms/solver.py
+++ algorithms/solver.py
@@ -1,5 +1,57 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from plant import Pendulum, Plant
+from sqp import SQP
+
+class Solver:
+    def __init__(self) -> None:
+        self.step = 0.1
+        self.stop = 10
+
+    def solve(self, plant:Plant, x0):
+        self.x = [x0]
+        self.y = [plant.h(x0, np.matrix(0), 0)]
+        u = np.matrix(0)
+        for t in np.arange(self.step, self.stop, self.step):
+            x = self.step*plant.f(self.x[-1], u, t)
+            self.x.append(x)
+            self.y.append(plant.h(x, u, t))
+
+    def plot(self):
+        t = np.arange(0, self.stop, self.step)
+        x = np.hstack(self.x).T
+        y = np.hstack(self.y).T
+        plt.plot(t, x)
+        plt.title('x')
+        plt.figure()
+        plt.plot(t, y)
+        plt.title('y')
+        plt.show()
+
+
+class ImplicitSolver(Solver):
+    def __init__(self) -> None:
+        self.sqp = SQP()
+        self.step = 0.5
+        self.stop = 10
+
+    def solve(self, plant: Plant, x0):
+        self.x = [x0]
+        self.y = [plant.h(x0, np.matrix(0), 0)]
+        def cost(x):
+            dx = self.x[-1] - x
+            return dx.T*dx
+        def equal(x):
+            xdot = plant.f(x, np.matrix(0), t)
+            return x - self.step*xdot
+        def bound(x):
+            return np.matrix(np.zeros((0,1)))
+        for t in np.arange(self.step, self.stop, self.step):
+            self.sqp.optimize(cost, self.x[-1].copy(), equal, bound)
+            x = self.sqp.results()
+            self.x.append(x)
+            self.y.append(plant.h(x, np.matrix(0), t))
+        return self.y
 
 def forward_euler(f, x0, h, n):
     xs = [x0]
@@ -26,10 +78,16 @@ def backward_euler(f, x0, h, n):
         xs.append(x)
     return xs
 
-h = 0.1
-tau = 0.01
-t = np.arange(0, 10, h)
-x = np.array(backward_euler(lambda x:-1/tau*x, 1, h, 99))
-# print(len(t), len(x))
-plt.plot(t, x)
-plt.show()
+# h = 0.1
+# tau = 0.01
+# t = np.arange(0, 10, h)
+# x = np.array(backward_euler(lambda x:-1/tau*x, 1, h, 99))
+# # print(len(t), len(x))
+# plt.plot(t, x)
+# plt.show()
+
+pendulum = Pendulum()
+# solver = Solver()
+solver = ImplicitSolver()
+solver.solve(pendulum, np.matrix([0.1,0.]).T)
+solver.plot()
