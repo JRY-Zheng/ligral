8c807edfd100c7df33fbb18e7ef592ed12a847bc
Junruoyu Zheng <zhengjry@outlook.com>
2021-03-15 17:24:00 +0800

    add: split calculator model into many models

---
+++ src/Component/Models/DotDiv.cs
@@ -0,0 +1,60 @@
+/* Copyright (C) 2019-2021 Junruoyu Zheng. Home page: https://junruoyu-zheng.gitee.io/ligral
+
+   Distributed under MIT license.
+   See file LICENSE for detail or copy at https://opensource.org/licenses/MIT
+*/
+
+using System.Collections.Generic;
+using MathNet.Numerics.LinearAlgebra;
+using System;
+
+namespace Ligral.Component.Models
+{
+    class DotDiv : Model
+    {
+        protected override string DocString
+        {
+            get
+            {
+                return "This model outputs the broadcast quotient of the two inputs";
+            }
+        }
+        private string type;
+        protected override void SetUpPorts()
+        {
+            InPortList.Add(new InPort("left", this));
+            InPortList.Add(new InPort("right", this));
+            OutPortList.Add(new OutPort("result", this));
+        }
+        public override void Check()
+        {
+            try
+            {
+                (int xRowNo, int xColNo) = MatrixIteration.BroadcastShape(InPortList[0].RowNo, InPortList[0].ColNo, InPortList[1].RowNo, InPortList[1].ColNo);
+                OutPortList[0].SetShape(xRowNo, xColNo);
+            }
+            catch (Exception e)
+            {
+                throw logger.Error(new ModelException(this, e.Message));
+            }
+        }
+        protected override List<Matrix<double>> Calculate(List<Matrix<double>> values)
+        {
+            try
+            {
+                Results[0] = values[0].DotDiv(values[1]);
+            }
+            catch (System.ArgumentException e)
+            {
+                string message = e.Message;
+                int indexOfParenthesis = message.IndexOf('(');
+                if (indexOfParenthesis>=0)
+                {
+                    message = message.Substring(0, indexOfParenthesis);
+                }
+                throw logger.Error(new ModelException(this, message));
+            }
+            return Results;
+        }
+    }
+}
