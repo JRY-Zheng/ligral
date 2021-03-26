c84a3d18e3dae0b0b9627c6e062f2e0c4b9d9a63
Junruoyu Zheng <zhengjry@outlook.com>
2021-03-26 10:16:22 +0800

    tst: cos and cosh model

---
+++ test/ModelComputation/TestCosh.cs
@@ -0,0 +1,53 @@
+/* Copyright (C) 2019-2021 Junruoyu Zheng. Home page: https://junruoyu-zheng.gitee.io/ligral
+
+   Distributed under MIT license.
+   See file LICENSE for detail or copy at https://opensource.org/licenses/MIT
+*/
+
+using System;
+using System.Collections.Generic;
+using Xunit;
+using MathNet.Numerics.LinearAlgebra;
+using Ligral.Component;
+
+namespace Ligral.Tests.ModelTester
+{
+    public class TestCosh
+    {
+        [Fact]
+        public void Cosh_InputZero_OutputSin()
+        {
+            var model = ModelManager.Create("Cosh");
+            var modelTester = new ModelTester();
+            var inputs = new List<Matrix<double>> {0.ToMatrix()};
+            var outputs = new List<Matrix<double>> {1.ToMatrix()};
+            Assert.True(modelTester.Test(model, inputs, outputs));
+        }
+        [Fact]
+        public void Cosh_InputNegative_OutputNegative()
+        {
+            var model = ModelManager.Create("Cosh");
+            var modelTester = new ModelTester();
+            var inputs = new List<Matrix<double>> {-10.ToMatrix()};
+            var outputs = new List<Matrix<double>> {Math.Cosh(-10).ToMatrix()};
+            Assert.True(modelTester.Test(model, inputs, outputs));
+        }
+        [Fact]
+        public void Cosh_InputPositive_OutputPositive()
+        {
+            var model = ModelManager.Create("Cosh");
+            var modelTester = new ModelTester();
+            var inputs = new List<Matrix<double>> {1.3.ToMatrix()};
+            var outputs = new List<Matrix<double>> {Math.Cosh(1.3).ToMatrix()};
+            Assert.True(modelTester.Test(model, inputs, outputs));
+        }
+        [Fact]
+        public void Cosh_InputMatrix_OutputMatrix()
+        {
+            var model = ModelManager.Create("Cosh");
+            var modelTester = new ModelTester();
+            var inputs = new List<Matrix<double>> {Matrix<double>.Build.DenseOfArray(new double[2,3]{{1, -2, 3}, {-4.707, 5.707, 100}})};
+            Assert.True(modelTester.Test(model, inputs, inputs.ConvertAll(matrix => matrix.Map(item => Math.Cosh(item)))));
+        }
+    }
+}
