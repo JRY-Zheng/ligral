3e821e87faad25def8b55d231da664d3cb0678f1
Junruoyu Zheng <zhengjry@126.com>
2021-04-10 23:18:08 +0800

    add: code ast and token

---
+++ src/Syntax/CodeASTs/CodeAST.cs
@@ -0,0 +1,12 @@
+/* Copyright (C) 2019-2021 Junruoyu Zheng. Home page: https://junruoyu-zheng.gitee.io/ligral
+
+   Distributed under MIT license.
+   See file LICENSE for detail or copy at https://opensource.org/licenses/MIT
+*/
+
+namespace Ligral.Syntax.CodeASTs
+{
+    class CodeAST 
+    {
+    }
+}
---
+++ src/Syntax/CodeASTs/CopyCodeAST.cs
@@ -0,0 +1,15 @@
+/* Copyright (C) 2019-2021 Junruoyu Zheng. Home page: https://junruoyu-zheng.gitee.io/ligral
+
+   Distributed under MIT license.
+   See file LICENSE for detail or copy at https://opensource.org/licenses/MIT
+*/
+
+
+namespace Ligral.Syntax.CodeASTs
+{
+    class CopyCodeAST : CodeAST 
+    {
+        public CodeToken Source;
+        public CodeToken Destination;
+    }
+}
