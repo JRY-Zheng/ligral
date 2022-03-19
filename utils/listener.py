1a1c4f14dfd3b70631eba80a0cd7d4db3b5069e5
Junruoyu Zheng <zhengjry@126.com>
2022-03-19 15:13:21 +0800

    add: udp sender and listener demo

--- utils/listener.py
+++ utils/listener.py
@@ -12,13 +12,8 @@ UDP_IP_ADDRESS = '127.0.0.1'
 UDP_PORT_NO = 8783
 
 serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-# serverSock.bind((UDP_IP_ADDRESS, UDP_PORT_NO))
+serverSock.bind((UDP_IP_ADDRESS, UDP_PORT_NO))
 for i in range(100):
-    jsonbytes = json.dumps({
-        "label": 0xffb0,
-        "data": {
-            "test": i,
-        }
-    }).encode('ascii')
-    serverSock.sendto(jsonbytes, (UDP_IP_ADDRESS, UDP_PORT_NO))
+    buf = serverSock.recvfrom(1024)
+    print(buf)
     sleep(0.1)
