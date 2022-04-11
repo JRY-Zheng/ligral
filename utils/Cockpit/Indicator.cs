fe2cca8949186bfab67580ee44652058a7eda329
Junruoyu Zheng <zhengjry@126.com>
2022-04-11 14:02:14 +0800

    add: indicator tick labels

--- utils/Cockpit/Indicator.cs
+++ utils/Cockpit/Indicator.cs
@@ -13,9 +13,19 @@ namespace Cockpit
         public double Window {get; set;}
         public double Interval {get; set;}
         public double CurrentValue {get; set;}
+        public double LongPosition1 {get; set;} = 0.3;
+        public double LongPosition2 {get; set;} = 0.9;
+        public double MediumPosition1 {get; set;} = 0.25;
+        public double MediumPosition2 {get; set;} = 0.75;
+        public double ShortPosition1 {get; set;} = 0.3;
+        public double ShortPosition2 {get; set;} = 0.7;
+        public double LabelPosition {get; set;} = 0.1;
         private List<double> LongTicks = new List<double>();
         private List<double> MediumTicks = new List<double>();
         private List<double> ShortTicks = new List<double>();
+        private List<double> LongTickValues = new List<double>();
+        private List<Line> lines = new List<Line>();
+        private List<TextBlock> labels = new List<TextBlock>();
         static Brush tickBrush = Brushes.White;
         public Indicator(Canvas canvas, double upper, double lower=0)
         {
@@ -35,18 +45,21 @@ namespace Cockpit
             ShortTicks.Clear();
             MediumTicks.Clear();
             LongTicks.Clear();
+            LongTickValues.Clear();
             while (tickValue < valueAtBottom)
             {
                 tickValue += step;
                 tickIndex ++;
             }
-            while (tickValue < valueAtTop && tickValue < UpperBound)
+            while (tickValue <= valueAtTop && tickValue <= UpperBound)
             {
                 double tick = (tickValue-valueAtTop)/(valueAtBottom-valueAtTop);
                 switch (tickIndex % 4)
                 {
                 case 0:
-                    LongTicks.Add(tick); break;
+                    LongTicks.Add(tick); 
+                    LongTickValues.Add(tickValue);
+                    break;
                 case 2:
                     MediumTicks.Add(tick); break;
                 case 1:
@@ -60,76 +73,86 @@ namespace Cockpit
         public void DrawLinearTicks()
         {
             int i = 0;
-            List<Line> unusedLine = new List<Line>();
-            foreach (var element in canvas.Children)
+            while (i < ShortTicks.Count + MediumTicks.Count + LongTicks.Count)
             {
-                if (element is Line line) 
+                Line line;
+                if (i < lines.Count)
                 {
-                    if (i<ShortTicks.Count)
-                    {
-                        line.X1 = canvas.ActualWidth*0.3;
-                        line.X2 = canvas.ActualWidth*0.7;
-                        line.Y1 = canvas.ActualHeight*ShortTicks[i];
-                        line.Y2 = canvas.ActualHeight*ShortTicks[i];
-                    }
-                    else if (i<ShortTicks.Count+MediumTicks.Count)
-                    {
-                        int ii = i - ShortTicks.Count;
-                        line.X1 = canvas.ActualWidth*0.25;
-                        line.X2 = canvas.ActualWidth*0.75;
-                        line.Y1 = canvas.ActualHeight*MediumTicks[ii];
-                        line.Y2 = canvas.ActualHeight*MediumTicks[ii];
-                    }
-                    else if (i<ShortTicks.Count+MediumTicks.Count+LongTicks.Count)
-                    {
-                        int ii = i - ShortTicks.Count - MediumTicks.Count;
-                        line.X1 = canvas.ActualWidth*0.1;
-                        line.X2 = canvas.ActualWidth*0.9;
-                        line.Y1 = canvas.ActualHeight*LongTicks[ii];
-                        line.Y2 = canvas.ActualHeight*LongTicks[ii];
-                    }
-                    else
-                    {
-                        unusedLine.Add(line);
-                    }
-                    i++;
+                    line = lines[i];
                 }
                 else
                 {
-                    continue;
+                    line = new Line() {Stroke = tickBrush, StrokeThickness = 1};
+                    lines.Add(line);
                 }
-            }
-            foreach (Line line in unusedLine)
-            {
-                canvas.Children.Remove(line);
-            }
-            while (i<ShortTicks.Count+MediumTicks.Count+LongTicks.Count)
-            {
-                Line line = new Line(){Stroke = tickBrush, StrokeThickness = 1};
                 if (i<ShortTicks.Count)
                 {
-                    line.X1 = canvas.ActualWidth*0.3;
-                    line.X2 = canvas.ActualWidth*0.7;
+                    line.X1 = canvas.ActualWidth*ShortPosition1;
+                    line.X2 = canvas.ActualWidth*ShortPosition2;
                     line.Y1 = canvas.ActualHeight*ShortTicks[i];
                     line.Y2 = canvas.ActualHeight*ShortTicks[i];
                 }
                 else if (i<ShortTicks.Count+MediumTicks.Count)
                 {
                     int ii = i - ShortTicks.Count;
-                    line.X1 = canvas.ActualWidth*0.25;
-                    line.X2 = canvas.ActualWidth*0.75;
-                    line.Y1 = canvas.ActualHeight*ShortTicks[ii];
-                    line.Y2 = canvas.ActualHeight*ShortTicks[ii];
+                    line.X1 = canvas.ActualWidth*MediumPosition1;
+                    line.X2 = canvas.ActualWidth*MediumPosition2;
+                    line.Y1 = canvas.ActualHeight*MediumTicks[ii];
+                    line.Y2 = canvas.ActualHeight*MediumTicks[ii];
+                }
+                else if (i<ShortTicks.Count+MediumTicks.Count+LongTicks.Count)
+                {
+                    int ii = i - ShortTicks.Count - MediumTicks.Count;
+                    line.X1 = canvas.ActualWidth*LongPosition1;
+                    line.X2 = canvas.ActualWidth*LongPosition2;
+                    line.Y1 = canvas.ActualHeight*LongTicks[ii];
+                    line.Y2 = canvas.ActualHeight*LongTicks[ii];
+                }
+                if (!canvas.Children.Contains(line))
+                {
+                    canvas.Children.Add(line);
+                }
+                i++;
+            }
+            while (i < lines.Count)
+            {
+                if (canvas.Children.Contains(lines[i]))
+                {
+                    canvas.Children.Remove(lines[i]);
+                }
+                i++;
+            }
+            i = 0;
+            while (i < LongTicks.Count)
+            {
+                TextBlock label;
+                if (i < labels.Count)
+                {
+                    label = labels[i];
+                    label.Text = LongTickValues[i].ToString();
+                    Canvas.SetTop(label, canvas.ActualHeight*LongTicks[i]-label.ActualHeight/2);
                 }
                 else
                 {
-                    int ii = i - ShortTicks.Count - MediumTicks.Count;
-                    line.X1 = canvas.ActualWidth*0.1;
-                    line.X2 = canvas.ActualWidth*0.8;
-                    line.Y1 = canvas.ActualHeight*ShortTicks[ii];
-                    line.Y2 = canvas.ActualHeight*ShortTicks[ii];
+                    label = new TextBlock();
+                    labels.Add(label);
+                    label.Text = LongTickValues[i].ToString();
+                    Canvas.SetTop(label, canvas.ActualHeight*LongTicks[i]-label.ActualHeight/2);
+                    Canvas.SetLeft(label, LabelPosition);
+                    label.Foreground = tickBrush;
                 }
-                canvas.Children.Add(line);
+                if (!canvas.Children.Contains(label))
+                {
+                    canvas.Children.Add(label);
+                }
+                i++;
+            }
+            while (i < labels.Count)
+            {
+                if (canvas.Children.Contains(labels[i]))
+                {
+                    canvas.Children.Remove(labels[i]);
+                }
                 i++;
             }
         }
