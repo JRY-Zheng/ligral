d7b43883d1e7acb15b01dc2e4a51002f0253f2f4
Junruoyu Zheng <zhengjry@126.com>
2022-05-18 10:22:07 +0800

    copyright: 2022

--- src/Component/Signature.cs
+++ src/Component/Signature.cs
@@ -1,4 +1,4 @@
-/* Copyright (C) 2019-2021 Junruoyu Zheng. Home page: https://junruoyu-zheng.gitee.io/ligral
+/* Copyright (C) 2019-2022 Junruoyu Zheng. Home page: https://junruoyu-zheng.gitee.io/ligral
 
    Distributed under MIT license.
    See file LICENSE for detail or copy at https://opensource.org/licenses/MIT
