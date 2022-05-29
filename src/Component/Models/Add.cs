c06b58f0d9d622076c3eaaf70881cdfff577d58d
Junruoyu Zheng <zhengjry@126.com>
2022-05-29 18:03:28 +0800

    add: modify add char size to fit codes

--- src/Component/Models/Add.cs
+++ src/Component/Models/Add.cs
@@ -55,5 +55,27 @@ namespace Ligral.Component.Models
             }
             return Results;
         }
+        public override List<int> GetCharacterSize()
+        {
+            // #define BROADCAST_RCRC 0
+            // #define BROADCAST_RC11 1
+            // #define BROADCAST_11RC 2
+            // #define BROADCAST_RCR1 3
+            // #define BROADCAST_R1RC 4
+            // #define BROADCAST_RC1C 5
+            // #define BROADCAST_1CRC 6
+            int type = 0;
+            int LR = InPortList[0].RowNo;
+            int LC = InPortList[0].ColNo;
+            int RR = InPortList[1].RowNo;
+            int RC = InPortList[1].ColNo;
+            if (RR == 1 && RC == 1) type = 1;
+            else if (LR == 1 && LC == 1) type = 2;
+            else if (RR != 1 && RC == 1) type = 3;
+            else if (LR != 1 && LC == 1) type = 4;
+            else if (RR == 1 && RC != 1) type = 5;
+            else if (LR == 1 && LC != 1) type = 6;
+            return new List<int>() {OutPortList[0].RowNo, OutPortList[0].ColNo, type};
+        }
     }
 }
