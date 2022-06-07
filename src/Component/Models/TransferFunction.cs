692c78684ade9261207b3e576c58c06f5b629763
Junruoyu Zheng <zhengjry@126.com>
2022-06-07 11:52:34 +0800

    fix: matrix A error

--- src/Component/Models/TransferFunction.cs
+++ src/Component/Models/TransferFunction.cs
@@ -123,7 +123,7 @@ namespace Ligral.Component.Models
             A = Matrix<double>.Build.Dense(N-1, N-1, 0);
             for (int i=0; i<N-1; i++)
             {
-                A[N-2, N-2-i] = -denominator[0, i+1];
+                A[N-2-i, N-2] = -denominator[0, i+1];
             }
             for (int i=1; i<N-1; i++)
             {
