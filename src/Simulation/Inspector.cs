dfcb9263554045de78821e8eb92c07ec92660b73
Junruoyu Zheng <zhengjry@126.com>
2022-06-08 12:26:21 +0800

    fix: nodes order affects shape guessing behavior

--- src/Simulation/Inspector.cs
+++ src/Simulation/Inspector.cs
@@ -4,6 +4,7 @@
    See file LICENSE for detail or copy at https://opensource.org/licenses/MIT
 */
 
+using System.Linq;
 using System.Collections.Generic;
 using Ligral.Component;
 
@@ -16,6 +17,7 @@ namespace Ligral.Simulation
         private Logger logger = new Logger("Inspector");
         private void Visit(Model node, bool allowGuess)
         {
+            logger.Debug($"Visiting {node.ScopedName}, allow guess: {allowGuess}");
             if (!allNodes.Contains(node))
             {
                 throw logger.Error(new LigralException($"Schematic Error: {node.ScopedName} is not in inspection list."));
@@ -26,24 +28,38 @@ namespace Ligral.Simulation
                 {
                     try
                     {
+                        logger.Debug($"Checking {node.ScopedName}");
                         node.Check();
                     }
-                    catch (LigralException)
+                    catch (LigralException e)
                     {
                         if (!allowGuess)
                         {
                             logger.Solve();
+                            logger.Debug($"Fail to check {node.ScopedName}: {e.Message}");
+                            if (node is InitializeableModel iNode) 
+                            {
+                                iNode.ResetCheck();
+                                logger.Debug($"Uncheck {iNode.ScopedName}");
+                            }
                             return;
                         }
                         throw logger.Error(new LigralException("Ligral cannot guess the correct shape"));
                     }
-                    if (node is InitializeableModel i && i.Guessing && !allowGuess) return;
+                    if (node is InitializeableModel i && i.Guessing && !allowGuess) 
+                    {
+                        i.ResetCheck();
+                        logger.Debug($"Uncheck {i.ScopedName}");
+                        return;
+                    }
                     routine.Add(node);
+                    logger.Debug($"Add {node.ScopedName} to routine");
                     node.Inspect().ForEach(subNode=>Visit(subNode, allowGuess));
                 }
             }
             else if (node is InitializeableModel initializeableModel)
             {
+                logger.Debug($"Initialize {node.ScopedName}");
                 initializeableModel.Initialize();
                 Visit(initializeableModel, allowGuess);
             }
@@ -65,23 +81,36 @@ namespace Ligral.Simulation
             {
                 Visit(node, false);
             }
-            foreach(Model node in nodes)
+            var leftNodes = nodes.Except(routine);
+            int leftNodesCount = leftNodes.Count();
+            while (true)
             {
-                if (routine.Contains(node)) continue;
-                Visit(node, false);
+                foreach(Model node in leftNodes)
+                {
+                    // if (routine.Contains(node)) continue;
+                    Visit(node, false);
+                }
+                leftNodes = nodes.Except(routine);
+                if (leftNodes.Count() == leftNodesCount) break;
+                leftNodesCount = leftNodes.Count();
+                if (leftNodesCount == 0) break;
+                logger.Warn($"Ligral cannot determine all shape this time, {leftNodesCount} nodes ({string.Join(", ", leftNodes.Select(node=>node.ScopedName))}) was left, will try again.");
             }
-            foreach(Model node in nodes)
+            foreach(Model node in leftNodes)
             {
-                if (routine.Contains(node)) continue;
+                // if (routine.Contains(node)) continue;
                 if (node is InitializeableModel i)
                 {
                     i.ResetCheck();
                 }
             }
-            foreach(Model node in nodes)
+            if (leftNodesCount > 0)
             {
-                if (routine.Contains(node)) continue;
-                Visit(node, true);
+                logger.Warn($"Try to guess shape of {leftNodesCount} nodes: {string.Join(", ", leftNodes.Select(node=>node.ScopedName))}");
+                foreach(Model node in leftNodes)
+                {
+                    Visit(node, true);
+                }
             }
             // System.Console.WriteLine(string.Join(", ", routine.ConvertAll(node=>node.Name)));
             if (routine.Count != nodes.Count)
