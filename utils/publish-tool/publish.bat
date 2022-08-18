6686c6a78882b01d248c1384088131483d3a362f
Junruoyu Zheng <zhengjry@126.com>
2022-08-18 09:37:51 +0800

    mod: use more common command instead

--- utils/publish-tool/publish.bat
+++ utils/publish-tool/publish.bat
@@ -5,12 +5,12 @@ dotnet publish src -p:PublishSingleFile=true -p:UseAppHost=true -o bin/Publish/l
 @REM dotnet publish -p:PublishSingleFile=true -o bin/Publish/win10 -c Release --self-contained true --version-suffix beta -r win10-x64
 @REM dotnet publish -p:PublishSingleFile=true -o bin/Publish/linux -c Release --self-contained true --version-suffix beta -r linux-x64
 
-cp -r utils\publish-tool\linux\ligral-dpkg\ bin\Publish\linux\
-cp bin\Publish\linux\ligral bin\Publish\linux\ligral-v0.2.3-linux-x64
-cp bin\Publish\linux\ligral bin\Publish\linux\ligral-dpkg\usr\bin\ligral-v0.2.3\ligral-v0.2.3-linux-x64
-cp bin\Debug\netcoreapp3.1\default.lig bin\Publish\linux\ligral-dpkg\usr\bin\ligral-v0.2.3\default.lig
-cp -r bin\Debug\netcoreapp3.1\plugins bin\Publish\linux\ligral-dpkg\usr\bin\ligral-v0.2.3\plugins
+xcopy /s /i /y utils\publish-tool\linux\ligral-dpkg\ bin\Publish\linux\ligral-dpkg\
+copy bin\Publish\linux\ligral bin\Publish\linux\ligral-v0.2.3-linux-x64
+copy bin\Publish\linux\ligral bin\Publish\linux\ligral-dpkg\usr\bin\ligral-v0.2.3\ligral-v0.2.3-linux-x64
+copy bin\Debug\netcoreapp3.1\default.lig bin\Publish\linux\ligral-dpkg\usr\bin\ligral-v0.2.3\default.lig
+xcopy /s /i /y bin\Debug\netcoreapp3.1\plugins bin\Publish\linux\ligral-dpkg\usr\bin\ligral-v0.2.3\plugins
 
-cp bin\Publish\win10\ligral.exe bin\Publish\win10\ligral-v0.2.3-win10-x64.exe
-cp bin\Debug\netcoreapp3.1\default.lig bin\Publish\win10\default.lig
-cp -r bin\Debug\netcoreapp3.1\plugins bin\Publish\win10\plugins
+copy bin\Publish\win10\ligral.exe bin\Publish\win10\ligral-v0.2.3-win10-x64.exe
+copy bin\Debug\netcoreapp3.1\default.lig bin\Publish\win10\default.lig
+xcopy /s /i /y bin\Debug\netcoreapp3.1\plugins bin\Publish\win10\plugins
